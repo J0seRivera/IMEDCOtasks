3056d2921d4675dab88927a52c19afbee265583a
Jose_Rivera <joseriveradl@gmail.com>
2020-04-13 20:50:54 -0600

    tareas Semana 4

--- jsWeek.js
+++ jsWeek.js
@@ -1,6 +1,5 @@
-alert("Disfruten la Semana Santa, no dejaré tareas 🧐");
 $(document).ready(function(){
       $('html, body').animate({
         scrollTop: '2800px'
-      }, 1800)
+      }, 1650)
 });
